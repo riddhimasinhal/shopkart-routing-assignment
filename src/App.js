078fa669c4ffe161963c11ec6ccc879e4ebf7f21
Riddhimasinhal <riddhimasinhal08@gmail.com>
2026-05-08 14:55:36 +0530

    Fix App.js routing, add scripts to package.json, remove node_modules from git tracking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
@@ -7,19 +8,22 @@ import Home from "./pages/Home";
 // TODO: Import React Router
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <BrowserRouter>
+       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       {/* TODO: Implement Routing Here */}
       
-      <Home />
+      {/* <Home /> */}
+      </BrowserRouter>
     </div>
   );
 }
