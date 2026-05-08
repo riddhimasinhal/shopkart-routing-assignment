5a5e8a9e1ef934d54600fada55001976fc592245
Riddhimasinhal <riddhimasinhal08@gmail.com>
2026-05-08 14:20:19 +0530

    Completed ShopKart Routing Assignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+
 // TODO: Import About
 // TODO: Import Contact
 // TODO: Import React Router
-
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
       <Navbar />
-      
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
       {/* TODO: Implement Routing Here */}
       
       <Home />
